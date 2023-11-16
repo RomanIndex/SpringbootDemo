b6de7311999bb9009ea6ddb0ada8e5c0c20257ec
RomanIndex <1348275874@qq.com>
2023-11-17 01:22:51 +0800

    commit in 2023.11.17 2

--- demo-temp/src/main/java/com/zz/demo/temp/main/TestGit1117.java
+++ demo-temp/src/main/java/com/zz/demo/temp/main/TestGit1117.java
@@ -1,7 +1,7 @@
-package com.zz.demo.temp;
+package com.zz.demo.temp.main;
 
 public class TestGit1117 {
     public static void main(String[] args) {
-
+        System.out.println("NOW!");
     }
 }
